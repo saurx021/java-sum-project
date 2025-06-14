a055a725573783c065d3fa1ce9aa7e9d250f2c85
Saurabh <saurx021@gmail.com>
2025-06-15 02:29:12 +0530

    Initial commit of sum of three nums

---
+++ SumLargest.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class SumLargest{
+public static void main(String[]args){
+Scanner sc = new Scanner(System.in);
+System.out.println("Enter First number");
+int a = sc.nextInt();
+System.out.println("Enter second number");
+int b = sc.nextInt();
+System.out.println("Enter third number");
+int c = sc.nextInt();
+
+int largest = Math.max(a,Math.max(b,c));
+System.out.println("Largest of three is "+largest);
+System.out.println("Sum with largest:"+(a+b+c));
+
+}
+
+
+}
